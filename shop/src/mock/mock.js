eae57ccac3e26f2880caf982bebc1f6dfc26ee00
me <oncwnuCnCfGr_gZ_2S2ooxv0pI7Q@git.weixin.qq.com>
2019-04-28 18:43:56 +0800

    运用了mock把商品列表mock了一下

---
+++ shop/src/mock/mock.js
@@ -0,0 +1,18 @@
+import Mock from "mockjs";
+
+let Random = Mock.Random;
+let getVarityItem = res => {
+  let productList = [];
+  for(let i=0; i<100; i++){
+      let product = {
+          name: Random.ctitle(5, 10),
+          img: Random.dataImage("200x200", '农机' + Random.integer(1, 100)),
+          price: Random.integer(1000, 9999),
+          owner: Random.cname()
+      };
+      productList.push(product);
+  }
+  return productList;
+};
+
+Mock.mock("https://www.zcq.com/getVarietyItem", getVarityItem());
---
+++ shop/src/service.config.js
@@ -0,0 +1,8 @@
+const MOCKURL = "https://www.zcq.com/"; //mock模拟
+const SERVER = ""; //真实接口
+
+const url = {
+    getVarietyItem: MOCKURL + 'getVarietyItem'
+};
+
+export default url;
